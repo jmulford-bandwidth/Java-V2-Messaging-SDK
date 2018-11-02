e9c546a7e800a1aac88788fadab5bd01e4773601
jmulford-bandwidth <jmulford@bandwidth.com>
2018-11-02 11:25:32 -0400

    Allowed SendMessageRequestBody to accept a single string for "to" parameter

--- src/test/java/com/bandwidth/V2/models/SendMessageRequestBodyTest.java
+++ src/test/java/com/bandwidth/V2/models/SendMessageRequestBodyTest.java
@@ -21,15 +21,67 @@ import java.util.ArrayList;
 public class SendMessageRequestBodyTest {
 
     /**
-     * Test for proper functionality of SendMessageRequestBody.toJSON()
+     * Test for proper functionality of SendMessageRequestBody.toJSON() with multiple recipients
      */
     @Test
-    public void testToJSON() throws JsonProcessingException {
+    public void testToJSONMultipleRecipients() throws JsonProcessingException {
         //Build custom test values
         String from = "123";
         ArrayList<String> to = new ArrayList<String>();
-        String toAdd = "456";
-        to.add(toAdd);
+        String toAdd1 = "456";
+        String toAdd2 = "567";
+        to.add(toAdd1);
+        to.add(toAdd2);
+        String text = "789";
+        String applicationId = "321";
+        ArrayList<String> media = new ArrayList<String>();
+        String mediaAdd = "654";
+        media.add(mediaAdd);
+        String tag = "987";
+
+        String expectedJSON = "{\"to\":[\"456\",\"567\"],\"from\":\"123\",\"text\":\"789\",\"applicationId\":\"321\",\"media\":[\"654\"],\"tag\":\"987\"}";
+
+        //Create SendMessageRequestBody and call toJSON()
+        SendMessageRequestBody requestBody = new SendMessageRequestBody(to, from, text, applicationId, media, tag);
+        String json = requestBody.toJSON();
+
+        //Check for correct value returned
+        assertTrue(json.equals(expectedJSON));
+    }
+
+    /**
+     * Test for exclusion of null values in toJSON()
+     */
+    @Test
+    public void testToJSONNullValues() throws JsonProcessingException {
+        //Test variables
+        String from = "123";
+        ArrayList<String> to = new ArrayList<String>();
+        String toAdd1 = "456";
+        String toAdd2 = "567";
+        to.add(toAdd1);
+        to.add(toAdd2);
+        String text = "789";
+        String applicationId = "321";
+
+        String expectedJSON = "{\"to\":[\"456\",\"567\"],\"from\":\"123\",\"text\":\"789\",\"applicationId\":\"321\"}";
+
+        //Create SendMessageRequestBody and call toJSON()
+        SendMessageRequestBody requestBody = new SendMessageRequestBody(to, from, text, applicationId, null, null);
+        String json = requestBody.toJSON();
+
+        //Check for correct value returned
+        assertTrue(json.equals(expectedJSON));
+    }
+
+    /**
+     * Test for proper functionality of SendMessageRequestBody.toJSON() with 1 recipient
+     */
+    @Test
+    public void testToJSONOneRecipient() throws JsonProcessingException {
+        //Build custom test values
+        String from = "123";
+        String to = "456";
         String text = "789";
         String applicationId = "321";
         ArrayList<String> media = new ArrayList<String>();
@@ -47,19 +99,4 @@ public class SendMessageRequestBodyTest {
         assertTrue(json.equals(expectedJSON));
     }
 
-    /**
-     * Test for exclusion of null values in toJSON()
-     */
-    @Test
-    public void testToJSONNullValues() throws JsonProcessingException {
-        //Test variables
-        String expectedJSON = "{}";
-
-        //Create SendMessageRequestBody and call toJSON()
-        SendMessageRequestBody requestBody = new SendMessageRequestBody(null, null, null, null, null, null);
-        String json = requestBody.toJSON();
-
-        //Check for correct value returned
-        assertTrue(json.equals(expectedJSON));
-    }
 }
